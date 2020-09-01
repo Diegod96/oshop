a98b89e9c277a209cc10605de907a52d91cb3945
DESKTOP-ALV8CGG\diego <diego.delgado@comcast.net>
2020-09-01 17:52:02 -0400

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDrv7cZDQkMCSO1kXlAx01fDI5-qAeDbjE",
+    authDomain: "oshop-c59f9.firebaseapp.com",
+    databaseURL: "https://oshop-c59f9.firebaseio.com",
+    projectId: "oshop-c59f9",
+    storageBucket: "oshop-c59f9.appspot.com",
+    messagingSenderId: "572382810966",
+    appId: "1:572382810966:web:97fdfe17b43caf7e5bfcf7",
+    measurementId: "G-P6YPJLTBV8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDrv7cZDQkMCSO1kXlAx01fDI5-qAeDbjE",
+    authDomain: "oshop-c59f9.firebaseapp.com",
+    databaseURL: "https://oshop-c59f9.firebaseio.com",
+    projectId: "oshop-c59f9",
+    storageBucket: "oshop-c59f9.appspot.com",
+    messagingSenderId: "572382810966",
+    appId: "1:572382810966:web:97fdfe17b43caf7e5bfcf7",
+    measurementId: "G-P6YPJLTBV8"
+  }
 };
 
 /*
